2ea35c9f01d6bc1c10abc9d1efdf8ebe0b573e68
Sergio Pecan <u20201a938@upc.edu.pe>
2024-05-25 16:26:47 -0500

    init commit

---
+++ src/main/java/com/rutaclara/rutaclara/RutaClaraApplication.java
@@ -0,0 +1,13 @@
+package com.rutaclara.rutaclara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RutaClaraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RutaClaraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rutaclara/rutaclara/RutaClaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rutaclara.rutaclara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RutaClaraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
